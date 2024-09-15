db27a37b3cfbd8be027231cf61827d8a4e15c1e8
Vinicius <viniciussantanna2005@gmail.com>
2024-09-15 20:00:16 -0300

    Header

---
+++ src/Components/Header/Header.styled.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const StyledNav = styled.nav `
+    display: flex;
+    justify-content: space-around;
+    gap: 250px;
+    color: #545454;
+    text-decoration: none;
+    align-items: center;
+`
+
+export const StyledHeader = styled.header`
+    margin-top: 39px;
+    margin-left: 45px;
+    margin-right: 30px;
+`
+
+export const StyledGuardIAn = styled.h1`
+    color: #545454;
+    font-size: 24px;
+`
+
+export const StyledIA = styled.strong`
+    color: #0055CF;
+    font-size: 24px;
+`
+
+export const StyledIA_pequeno = styled.strong`
+    color: #0055CF;
+`
+
+export const StyledLogin = styled.div `
+    border: solid 1px #545454;
+    width: 88px;
+    height: 35px;
+    align-items: center;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+`
+
+export const StyledLink = styled.nav`
+
+    a{
+        text-decoration: none;
+        color: #545454;
+    }
+`
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,26 @@
-export default function Header () {
+import { Link } from "react-router-dom";
+import { StyledGuardIAn, StyledHeader, StyledIA, StyledIA_pequeno, StyledLink, StyledLogin, StyledNav } from "./Header.styled";
+
+
+export const Header = () => {
     return(
         <>
-        
+            <StyledHeader>
+                <StyledNav>
+                    <StyledGuardIAn>Guard<StyledIA>IA</StyledIA>n</StyledGuardIAn>
+                    <StyledLink>
+                        <Link to="">Sobre o produto</Link>
+                    </StyledLink>
+                    <StyledLink>
+                        <Link to="">Cr<StyledIA_pequeno>IA</StyledIA_pequeno>dores</Link>
+                    </StyledLink>
+                    <StyledLogin>
+                        <StyledLink>
+                            <Link to="">Login</Link>
+                        </StyledLink>
+                    </StyledLogin>
+                </StyledNav>
+            </StyledHeader>
         </>
     )
 }
