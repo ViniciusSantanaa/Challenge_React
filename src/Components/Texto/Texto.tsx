7dd39623163a8deaf48a4444963f122d699215fb
Vinicius <viniciussantanna2005@gmail.com>
2024-09-15 20:01:03 -0300

    Pasta Texto

---
+++ src/Components/Texto/Texto.tsx
@@ -0,0 +1,10 @@
+export const Texto = () => {
+    return(
+        <main class="conteudo-seguro">
+        <section>
+            <p class="texto-seguro">Com nosso Seguro Automotivo, você pode ter a tranquilidade de saber que seu investimento está protegido contra imprevistos que poderiam comprometer seu valor. Preserve o valor do seu veículo e dirija com confiança sabendo que estamos cuidando dele.</p>
+        </section>
+        <img src="img/civic-site.png" alt="imagem site" class="imagem-carro">
+    </main>
+    )
+}
