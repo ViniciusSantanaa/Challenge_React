6f1f95822fd668dc6ab875c69c50442f4ab7f42a
Vinicius <viniciussantanna2005@gmail.com>
2024-09-15 20:44:06 -0300

    Ajuste de nome - Header

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Header } from "../../Components/Header/Header";
+import { Header } from "../../Components/Header/Header.Home";
 import { Main } from "../../Components/Main/Main";
 import { Texto } from "../../Components/Texto/Texto";
 import { Vantagens } from "../../Components/Vantagens/Vantagens";
