66a2c89e27b4fd72ceda46947598aad50477757e
Vinicius <viniciussantanna2005@gmail.com>
2024-09-15 20:00:52 -0300

    Home

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import { Header } from "../../Components/Header/Header";
+import { Main } from "../../Components/Main/Main";
+import { Vantagens } from "../../Components/Vantagens/Vantagens";
+
+
+export default function Home() {
+    return (
+        <>
+            <Header/>
+            <Main />
+            <Vantagens />
+        </>
+    );
+};
