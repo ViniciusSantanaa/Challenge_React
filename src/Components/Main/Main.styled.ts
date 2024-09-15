4ea8ddc3f9fc9e68e316e5d7b4daddb2463353c5
Vinicius <viniciussantanna2005@gmail.com>
2024-09-15 20:00:45 -0300

    Main

---
+++ src/Components/Main/Main.styled.ts
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const StyledMain = styled.main`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 0%;
+`
+
+export const StyledSection = styled.section`
+    width: 632px;
+    display: flex;
+    flex-direction: column;
+    gap: 0px;
+    margin-left: 56px;
+`
+
+export const StyledSlogan = styled.h2`
+    color: #545454;
+    margin-top: 133px;
+    font-size: 36px;
+    width: 632px;
+    height: 130px;
+`
+
+export const StyledContrate = styled.div`
+    background-color: #545454;
+    color: white;
+    width: 88px;
+    height: 35px;
+    align-items: center;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    margin-left: 247px;
+    width: 142px;
+    height: 55px;
+    font-size: 20px;
+`
+
+export const StyledContrateLink = styled.nav`
+    a{
+        text-decoration: none;
+        color: #ffff;
+    }
+`
+
+export const StyledIMG = styled.img`
+    width: 600px;
+    height: 400px;
+    margin-right: 30px;
+    margin-top: 155px;
+    border-radius: 10px;
+`
