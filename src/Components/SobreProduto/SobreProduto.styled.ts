e7e14c6253cb587c7dd9bb4e4221237f6f31a97b
Vinicius <viniciussantanna2005@gmail.com>
2024-09-16 14:35:48 -0300

    Sobre o Produto Responsividade completa

--- src/Components/SobreProduto/SobreProduto.styled.ts
+++ src/Components/SobreProduto/SobreProduto.styled.ts
@@ -4,6 +4,26 @@ export const StyledSectionProduto = styled.section`
     width: 1040px;
     align-items: center;
     margin-left: 130px;
+
+    @media (max-width: 430px) {
+        width: 390px;
+        margin-left: 29px;
+    }
+
+    @media (min-width: 431px) and (max-width: 744px) {
+        width: 500px;
+        margin-left: 29px;
+    }
+
+    @media (min-width: 745px) and (max-width: 834px) {
+        width: 721px;
+        margin-left: 68px;
+    }
+
+    @media (min-width: 835px) and (max-width: 1024px) {
+        width: 980px;
+        margin-left: 53px;
+    }
 `
 
 export const StyledSobreProduto = styled.p`
