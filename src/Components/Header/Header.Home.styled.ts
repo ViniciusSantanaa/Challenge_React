eff31959905c8437f69ed5be1e44b2010600e24d
Vinicius <viniciussantanna2005@gmail.com>
2024-09-16 13:29:22 -0300

    Header Home responsividade Completa

--- src/Components/Header/Header.Home.styled.ts
+++ src/Components/Header/Header.Home.styled.ts
@@ -7,22 +7,103 @@ export const StyledNav = styled.nav `
     color: #545454;
     text-decoration: none;
     align-items: center;
+
+    @media (max-width: 430px) {
+        gap: 15px;
+        font-size: 13px;
+    }
+
+    @media (min-width: 431px) and (max-width: 744px) {
+        gap: 75px;
+        font-size: 13px;
+    }
+
+    @media (min-width: 745px) and (max-width: 834px) {
+        gap: 75px;
+        font-size: 15px;
+    }
+
+    @media (min-width: 835px) and (max-width: 1024px) {
+        font-size: 20px;
+        gap: 82px;
+    }
 `
 
 export const StyledHeader = styled.header`
     margin-top: 39px;
     margin-left: 45px;
     margin-right: 30px;
+
+    @media (max-width: 430px) {
+        margin-top: 39px;
+        margin-left: 45px;
+        margin-right: 30px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    @media (min-width: 431px) and (max-width: 744px) {
+        margin-top: 39px;
+        margin-left: 45px;
+        margin-right: 30px;
+    }
+
+    @media (min-width: 745px) and (max-width: 834px) {
+        margin-top: 22px;
+        margin-left: 27px;
+        margin-right: 43px;
+    }
+
+    @media (min-width: 835px) and (max-width: 1024px) {
+        margin-top: 39px;
+        margin-left: 45px;
+        margin-right: 30px;
+    }
 `
 
 export const StyledGuardIAn = styled.h1`
     color: #545454;
     font-size: 24px;
+
+    @media (max-width: 430px){
+        color: #545454;
+        font-size: 20px;
+    }
+
+    @media (min-width: 431px) and (max-width: 744px) {
+        font-size: 20px;
+    }
+
+    @media (min-width: 745px) and (max-width: 834px) {
+        font-size: 24px;
+    }
+
+    @media (min-width: 835px) and (max-width: 1024px) {
+        font-size: 20px;
+    }
+
 `
 
 export const StyledIA = styled.strong`
     color: #0055CF;
     font-size: 24px;
+
+    @media (max-width: 430px){
+        font-size: 20px;
+    }
+
+    @media (min-width: 431px) and (max-width: 744px) {
+        font-size: 20px;
+    }
+
+    @media (min-width: 745px) and (max-width: 834px) {
+        font-size: 24px;
+    }
+
+    @media (min-width: 835px) and (max-width: 1024px) {
+        font-size: 20px;
+    }
 `
 
 export const StyledIA_pequeno = styled.strong`
@@ -37,6 +118,30 @@ export const StyledLogin = styled.div `
     border-radius: 10px;
     display: flex;
     justify-content: center;
+
+    @media (max-width: 430px) {
+        width: 53px;
+        height: 22px;
+        border-radius: 5px;
+    }
+
+    @media (min-width: 431px) and (max-width: 744px) {
+        width: 53px;
+        height: 22px;
+        border-radius: 5px;
+    }
+
+    @media (min-width: 745px) and (max-width: 834px) {
+        width: 76px;
+        height: 28px;
+        border-radius: 5px;
+    }
+
+    @media (min-width: 835px) and (max-width: 1024px) {
+        width: 86px;
+        height: 38px;
+        border-radius: 5px;
+    }
 `
 
 export const StyledLink = styled.nav`
